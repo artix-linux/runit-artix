d9721360b14a25df45edbb0285fb00145b900896
Muhammad Herdiansyah <herdiansyah@netc.eu>
2018-01-11 18:10:16 +0700

    clean up /tmp

--- core-services/99-cleanup.sh
+++ core-services/99-cleanup.sh
@@ -7,5 +7,6 @@ fi
 if [ ! -e /var/log/btmp ]; then
 	install -m0600 -o root -g utmp /dev/null /var/log/btmp
 fi
-install -dm1777 /tmp/.X11-unix /tmp/.ICE-unix
+rm -rf /tmp
+install -dm1777 /tmp /tmp/.X11-unix /tmp/.ICE-unix
 rm -f /etc/nologin /forcefsck /forcequotacheck /fastboot
