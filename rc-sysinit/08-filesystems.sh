d123927a21795f99160f40a5eb15ac472bbfc2ab
udeved <artoo@cromnix.org>
2018-05-02 02:58:14 +0200

    fix stage1; comment out all optional fs

--- rc-sysinit/08-filesystems.sh
+++ rc-sysinit/08-filesystems.sh
@@ -5,51 +5,51 @@
 msg "Remounting rootfs read-only..."
 mount -o remount,ro / || emergency_shell
 
-if [ -x /sbin/dmraid -o -x /bin/dmraid ]; then
-    msg "Activating dmraid devices..."
-    dmraid -i -ay
-fi
+# if [ -x /sbin/dmraid -o -x /bin/dmraid ]; then
+#     msg "Activating dmraid devices..."
+#     dmraid -i -ay
+# fi
 
-if [ -x /sbin/lvmetad -o -x /bin/lvmetad ]; then
-    msg "Activating lvmetad..."
-    lvmetad -f || emergency_shell
-fi
+# if [ -x /sbin/lvmetad -o -x /bin/lvmetad ]; then
+#     msg "Activating lvmetad..."
+#     lvmetad -f || emergency_shell
+# fi
 
-if [ -e /etc/crypttab ]; then
-    msg "Activating encrypted devices..."
-    awk -f /etc/runit/crypt.awk /etc/crypttab
-
-    if [ -x /sbin/vgchange -o -x /bin/vgchange ]; then
-        msg "Activating LVM devices for dm-crypt..."
-        vgchange --sysinit -a y || emergency_shell
-    fi
-fi
+# if [ -e /etc/crypttab ]; then
+#     msg "Activating encrypted devices..."
+#     awk -f /etc/runit/crypt.awk /etc/crypttab
+#
+#     if [ -x /sbin/vgchange -o -x /bin/vgchange ]; then
+#         msg "Activating LVM devices for dm-crypt..."
+#         vgchange --sysinit -a y || emergency_shell
+#     fi
+# fi
 
 # if [ -x /sbin/vgchange -o -x /bin/vgchange ]; then
 #     msg "Activating LVM devices..."
 #     vgchange --sysinit -a y || emergency_shell
 # fi
 
-if [ -e /etc/zfs/zpool.cache -a -x /usr/bin/zfs ]; then
-    msg "Activating ZFS devices..."
-    zpool import -c /etc/zfs/zpool.cache -N -a
+# if [ -e /etc/zfs/zpool.cache -a -x /usr/bin/zfs ]; then
+#     msg "Activating ZFS devices..."
+#     zpool import -c /etc/zfs/zpool.cache -N -a
+#
+#     msg "Mounting ZFS file systems..."
+#     zfs mount -a
+#
+#     msg "Sharing ZFS file systems..."
+#     zfs share -a
+#
+#     # NOTE(dh): ZFS has ZVOLs, block devices on top of storage pools.
+#     # In theory, it would be possible to use these as devices in
+#     # dmraid, btrfs, LVM and so on. In practice it's unlikely that
+#     # anybody is doing that, so we aren't supporting it for now.
+# fi
 
-    msg "Mounting ZFS file systems..."
-    zfs mount -a
-
-    msg "Sharing ZFS file systems..."
-    zfs share -a
-
-    # NOTE(dh): ZFS has ZVOLs, block devices on top of storage pools.
-    # In theory, it would be possible to use these as devices in
-    # dmraid, btrfs, LVM and so on. In practice it's unlikely that
-    # anybody is doing that, so we aren't supporting it for now.
-fi
-
-if [ -x /bin/btrfs ]; then
-    msg "Activating btrfs devices..."
-    btrfs device scan || emergency_shell
-fi
+# if [ -x /bin/btrfs ]; then
+#     msg "Activating btrfs devices..."
+#     btrfs device scan || emergency_shell
+# fi
 
 [ -f /fastboot ] && FASTBOOT=1
 [ -f /forcefsck ] && FORCEFSCK="-f"
