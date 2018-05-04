3139aa4609187779319ad4013a988ba9ed98bbe8
Muhammad Herdiansyah <herdiansyah@netc.eu>
2018-05-05 05:30:11 +0700

    Fix cryptsetup lock on shutdown

--- rc/shutdown.d/10-cryptsetup.sh
+++ rc/shutdown.d/10-cryptsetup.sh
@@ -3,5 +3,5 @@ if [[ -f /etc/crypttab ]] && type -p cryptsetup >/dev/null; then
 	# Maybe someone has LVM on an encrypted block device
 	# executing an extra vgchange is errorless
 	[[ $USELVM = [Yy][Ee][Ss] ]] && vgchange --sysinit -a n &>/dev/null
-	read_crypttab do_lock
+	deactivate_crypt
 fi
