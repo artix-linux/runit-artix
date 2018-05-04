a47df21b08224285d1e0e423ede94648b06b582b
Muhammad Herdiansyah <herdiansyah@netc.eu>
2018-05-05 06:00:40 +0700

    Remove unmount tmpfs
    runit needs to read /run during shutdown

--- rc/shutdown.d/07-tmpfs.sh
+++
@@ -1,3 +0,0 @@
-# unmount any non-API partitions that are backed by swap; we don't want to
-# move their contents into memory (waste of time and might caues OOM).
-status "Unmounting swap-backed filesystems" umount -r -a -t tmpfs
