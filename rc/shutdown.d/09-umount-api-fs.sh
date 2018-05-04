989a3238671e9fb002e331315277f5b734df4b77
Muhammad Herdiansyah <herdiansyah@netc.eu>
2018-05-05 05:49:04 +0700

    Fix unmount command

--- rc/shutdown.d/09-umount-api-fs.sh
+++ rc/shutdown.d/09-umount-api-fs.sh
@@ -1,3 +1,3 @@
 run_hook shutdown_preumount
-status "Unmounting non-API filesystems" umount_all
+status "Unmounting non-API filesystems" umount -r -a -t nosysfs,noproc,nodevtmpfs,notmpfs
 run_hook shutdown_postumount
