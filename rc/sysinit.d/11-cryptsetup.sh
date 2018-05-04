5c42be61439df3c6aa738315251363fc0333b718
Muhammad Herdiansyah <herdiansyah@netc.eu>
2018-05-05 03:45:20 +0700

    Use crypt.awk instead

--- rc/sysinit.d/11-cryptsetup.sh
+++ rc/sysinit.d/11-cryptsetup.sh
@@ -1,6 +1,6 @@
 # Set up non-root encrypted partition mappings
 if [[ -f /etc/crypttab ]] && type -p cryptsetup >/dev/null; then
-	read_crypttab do_unlock
+	status "Activating encrypted devices" awk -f /etc/runit/rc/crypt.awk /etc/crypttab
 	# Maybe someone has LVM on an encrypted block device
 	activate_vgs
 fi
