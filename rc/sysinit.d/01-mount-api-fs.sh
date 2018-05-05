b3c76e6a59273e3ab30f9920662d98c63ac765a7
Muhammad Herdiansyah <herdiansyah@netc.eu>
2018-05-05 21:08:11 +0700

    Mount efivarfs only if EFI is detected

--- rc/sysinit.d/01-mount-api-fs.sh
+++ rc/sysinit.d/01-mount-api-fs.sh
@@ -8,7 +8,7 @@ mkdir -p /dev/{pts,shm}
 mountpoint -q /dev/pts || mount -t devpts devpts /dev/pts -o mode=0620,gid=5,nosuid,noexec
 mountpoint -q /dev/shm || mount -t tmpfs shm /dev/shm -o mode=1777,nosuid,nodev
 mountpoint -q /sys/kernel/security || mount -n -t securityfs securityfs /sys/kernel/security
-mountpoint -q /sys/firmware/efi/efivars || mount -n -t efivarfs -o ro efivarfs /sys/firmware/efi/efivars
+[ -d /sys/firmware/efi ] && (mountpoint -q /sys/firmware/efi/efivars || mount -n -t efivarfs -o ro efivarfs /sys/firmware/efi/efivars)
 mountpoint -q /sys/fs/cgroup || mount -o mode=0755 -t tmpfs cgroup /sys/fs/cgroup
 mountpoint -q /sys/fs/cgroup/openrc || mkdir -p /sys/fs/cgroup/openrc && mount -t cgroup -o none,name=openrc cgroup /sys/fs/cgroup/openrc
 awk '$4 == 1 { system("mountpoint -q /sys/fs/cgroup/" $1 " || { mkdir -p /sys/fs/cgroup/" $1 " && mount -t cgroup -o " $1 " cgroup /sys/fs/cgroup/" $1 " ;}" ) }' /proc/cgroups
